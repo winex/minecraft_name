fdf4abdb9caed340f295f829c8b5a930acf330eb
Roman Vasiyarov <rvasiyarov@gmail.com>
2010-12-16 03:00:28 +0300

    move debug output to Loader()

--- Loader.java
+++ Loader.java
@@ -5,7 +5,6 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 public class Loader extends OutputStream {
@@ -16,18 +15,10 @@ public class Loader extends OutputStream {
 
     public static void main(String[] args) throws Exception {
         Loader l = new Loader("file:Minecraft.jar", "net.minecraft.LauncherFrame");
-        l.load(args);
+        l.load();
     }
 
-    public void load(String[] args) throws Exception {
-        scriptwindow.addline("D args: " + Arrays.toString(args));
-        scriptwindow.addline("D os.name: " + System.getProperty("os.name"));
-        scriptwindow.addline("D user.home: " + System.getProperty("user.home"));
-        scriptwindow.addline("D %APPDATA%: " + System.getenv("APPDATA"));
-        scriptwindow.addline("D $HOME: " + System.getenv("HOME"));
-        scriptwindow.addline("D $USER: " + System.getenv("USER"));
-        //".minecraft";
-
+    public void load() throws Exception {
         scriptwindow.addline("= Loading: " + url);
         URLClassLoader cl = new URLClassLoader(new URL[]{new URL("jar:" + url + "!/")});
         Class cls = cl.loadClass(mainclass);
@@ -102,12 +93,19 @@ public class Loader extends OutputStream {
         PrintStream p = new PrintStream(this);
         //System.setErr(p);
         //System.setOut(p);
-        String name = JOptionPane.showInputDialog(null, "Minecraft Name:", "Input Name");
-        System.out.println(name);
+
         scriptwindow = new ScriptWindow("Minecraft Name Changer by NanoEntity, modified code from eXemplar, updated by winex");
-        scriptwindow.nameSetting(name);
         scriptwindow.addObject("loader", this, false);
         scriptwindow.setVisible(true);
+
+        scriptwindow.addline("D os.name: " + System.getProperty("os.name"));
+        scriptwindow.addline("D user.home: " + System.getProperty("user.home"));
+        scriptwindow.addline("D %APPDATA%: " + System.getenv("APPDATA"));
+        scriptwindow.addline("D $HOME: " + System.getenv("HOME"));
+        scriptwindow.addline("D $USER: " + System.getenv("USER"));
+
+        String name = JOptionPane.showInputDialog(null, "Minecraft Name:", "Input Name");
+        scriptwindow.nameSetting(name);
     }
 
     private String last = "";
